27002539f99cad455574d03418590ed4f75478a1
aartis1 <ara67776@uga.edu>
2023-05-05 14:22:42 -0400

    Update Setdb1 MO CnR analysis.sh
    make sure everything is connected

--- Setdb1 MO CnR analysis.sh
+++ Setdb1 MO CnR analysis.sh
@@ -1,2 +1,24 @@
 # CnR-setA-B
-analyzing CnR data from Katie's MO experiment 
+#analyzing CnR data from Katie's MO experiment
+#!/bin/bash
+#SBATCH --job-name=CnR setA-B MO data		                        # Job name
+#SBATCH --partition=batch		                            # Partition (queue) name
+#SBATCH --ntasks=1			                                # Single task job
+#SBATCH --cpus-per-task=2		                            # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --mem=40gb			                                # Total memory for job
+#SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --output=/home/ara67776/work/CnR/Setdb1/error			    # Location of standard output and error log files (replace cbergman with your myid)
+#SBATCH --mail-user=ara67776@uga.edu                    # Where to send mail (replace cbergman with your myid)
+#SBATCH --mail-type=ALL                            # Mail events (BEGIN, END, FAIL, ALL)
+
+
+#set output directory
+OUTDIR="/home/ara67776/work/CnR/Setdb1"
+
+#if output directory doesn't exist, create it
+if [ ! -d $OUTDIR ]
+then
+    mkdir -p $OUTDIR
+fi
+
+Ashley 
