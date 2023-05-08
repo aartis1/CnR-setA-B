ee9767d545b94df3fbe0b7fc918339dac47b04d8
aartis1 <ara67776@uga.edu>
2023-05-08 10:42:16 -0400

    downloading genomes
    downloading zebrafish reference genome (both fasta and gtf files)
    
    also downloading spike in for CnR analysis (e. coli and s. cerevisiae fasta)

--- Setdb1_MO_CnR analysis.sh
+++ Setdb1_MO_CnR analysis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=CnR setA-B MO data		                        # Job name
+#SBATCH --job-name=CnR_setab_mods		                        # Job name
 #SBATCH --partition=batch		                            # Partition (queue) name
 #SBATCH --ntasks=1			                                # Single task job
-#SBATCH --cpus-per-task=2		                            # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --cpus-per-task=6		                            # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=40gb			                                # Total memory for job
 #SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
 #SBATCH --output=/home/ara67776/work/CnR/Setdb1/error			    # Location of standard output and error log files (replace cbergman with your myid)
@@ -18,5 +18,11 @@ if [ ! -d $OUTDIR ]
 then
     mkdir -p $OUTDIR
 fi
+#download relevant reference genome and annotations
+curl -s https://ftp.ensembl.org/pub/release-109/fasta/danio_rerio/dna/Danio_rerio.GRCz11.dna_rm.primary_assembly.fa.gz | gunzip -c > $OUTDIR/danio-refseq.fa
+curl -s https://ftp.ensembl.org/pub/release-109/gtf/danio_rerio/Danio_rerio.GRCz11.109.gtf.gz | gunzip -c > $OUTDIR/danio_annotation.gtf
 
+#spike in for CnR
+ curl -s https://ftp.ensembl.org/pub/release-109/fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz | gunzip -c $OUTDIR/sacc_refseq.fa
+ curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/005/845/GCA_000005845.2_ASM584v2/GCA_000005845.2_ASM584v2_genomic.fna.gz | gunzip -c $OUTDIR/ecoli_refseq.fa
  
