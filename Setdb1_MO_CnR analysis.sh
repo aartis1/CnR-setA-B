f68e618427f0aec49f3473b039369dd8f5b62691
aartis1 <ara67776@uga.edu>
2023-05-08 10:43:13 -0400

    Update Setdb1_MO_CnR analysis.sh

--- Setdb1_MO_CnR analysis.sh
+++ Setdb1_MO_CnR analysis.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
-#SBATCH --job-name=CnR_setab_mods		                        # Job name
-#SBATCH --partition=batch		                            # Partition (queue) name
-#SBATCH --ntasks=1			                                # Single task job
-#SBATCH --cpus-per-task=6		                            # Number of cores per task - match this to the num_threads used by BLAST
-#SBATCH --mem=40gb			                                # Total memory for job
-#SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
-#SBATCH --output=/home/ara67776/work/CnR/Setdb1/error			    # Location of standard output and error log files (replace cbergman with your myid)
-#SBATCH --mail-user=ara67776@uga.edu                    # Where to send mail (replace cbergman with your myid)
-#SBATCH --mail-type=ALL                            # Mail events (BEGIN, END, FAIL, ALL)
+!/bin/bash
+SBATCH --job-name=CnR_setab_mods		                        # Job name
+SBATCH --partition=batch		                            # Partition (queue) name
+SBATCH --ntasks=1			                                # Single task job
+SBATCH --cpus-per-task=6		                            # Number of cores per task - match this to the num_threads used by BLAST
+SBATCH --mem=40gb			                                # Total memory for job
+SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
+SBATCH --output=/home/ara67776/work/CnR/Setdb1/error			    # Location of standard output and error log files (replace cbergman with your myid)
+SBATCH --mail-user=ara67776@uga.edu                    # Where to send mail (replace cbergman with your myid)
+SBATCH --mail-type=ALL                            # Mail events (BEGIN, END, FAIL, ALL)
 
 
 #set output directory
@@ -25,4 +25,3 @@ curl -s https://ftp.ensembl.org/pub/release-109/gtf/danio_rerio/Danio_rerio.GRCz
 #spike in for CnR
  curl -s https://ftp.ensembl.org/pub/release-109/fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz | gunzip -c $OUTDIR/sacc_refseq.fa
  curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/005/845/GCA_000005845.2_ASM584v2/GCA_000005845.2_ASM584v2_genomic.fna.gz | gunzip -c $OUTDIR/ecoli_refseq.fa
- 
