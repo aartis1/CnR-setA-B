c11e595f69110661b1068f0885f64e42d71cd85b
aartis1 <ara67776@uga.edu>
2023-05-08 12:08:22 -0400

    Update Setdb1_MO_CnR analysis.sh

--- Setdb1_MO_CnR analysis.sh
+++ Setdb1_MO_CnR analysis.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-SBATCH --job-name=CnR_setab_mods		                        # Job name
+SBATCH --job-name=CnR_setab_MO		                        # Job name
 SBATCH --partition=batch		                            # Partition (queue) name
 SBATCH --ntasks=1			                                # Single task job
 SBATCH --cpus-per-task=2		                            # Number of cores per task - match this to the num_threads used by BLAST
 SBATCH --mem=40gb			                                # Total memory for job
-SBATCH --time=30:00:00  		                            # Time limit hrs:min:sec
+SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
 SBATCH --output=/home/ara67776/work/CnR/Setdb1/error			    # Location of standard output and error log files (replace cbergman with your myid)
 SBATCH --mail-user=ara67776@uga.edu                    # Where to send mail (replace cbergman with your myid)
 SBATCH --mail-type=ALL                            # Mail events (BEGIN, END, FAIL, ALL)
