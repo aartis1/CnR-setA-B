e93e62f009263acfd72c4dc0aca856836e9a7da1
aartis1 <ara67776@uga.edu>
2023-06-13 10:58:06 -0400

    adding more memory for job to run
    changing 2G to 40G
    changing 2 cpus per task to 24 cpus per task
    changing time to 24 hours

--- Setdb1_MO_CnR analysis.sh
+++ Setdb1_MO_CnR analysis.sh
@@ -2,9 +2,9 @@
 SBATCH --job-name=CnR_setab_MO		                        # Job name
 SBATCH --partition=batch		                            # Partition (queue) name
 SBATCH --ntasks=1			                                # Single task job
-SBATCH --cpus-per-task=2		                            # Number of cores per task - match this to the num_threads used by BLAST
-SBATCH --mem=2G			                                # Total memory for job
-SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
+SBATCH --cpus-per-task=24		                            # Number of cores per task - match this to the num_threads used by BLAST
+SBATCH --mem=40G			                                # Total memory for job
+SBATCH --time=24:00:00  		                            # Time limit hrs:min:sec
 SBATCH --output=/home/ara67776/work/CnR/Setdb1/error			    # Location of standard output and error log files (replace cbergman with your myid)
 SBATCH --mail-user=ara67776@uga.edu                    # Where to send mail (replace cbergman with your myid)
 SBATCH --mail-type=ALL                            # Mail events (BEGIN, END, FAIL, ALL)
@@ -18,9 +18,9 @@ if [ ! -d $OUTDIR ]
 then
     mkdir -p $OUTDIR
 fi
-#download relevant reference genome and annotations
-curl -s https://ftp.ensembl.org/pub/release-109/fasta/danio_rerio/dna/Danio_rerio.GRCz11.dna_rm.primary_assembly.fa.gz | gunzip -c > $OUTDIR/danio-refseq.fa
-curl -s https://ftp.ensembl.org/pub/release-109/gtf/danio_rerio/Danio_rerio.GRCz11.109.gtf.gz | gunzip -c > $OUTDIR/danio_annotation.gtf
+# #download relevant reference genome and annotations
+# curl -s https://ftp.ensembl.org/pub/release-109/fasta/danio_rerio/dna/Danio_rerio.GRCz11.dna_rm.primary_assembly.fa.gz | gunzip -c > $OUTDIR/danio-refseq.fa
+# curl -s https://ftp.ensembl.org/pub/release-109/gtf/danio_rerio/Danio_rerio.GRCz11.109.gtf.gz | gunzip -c > $OUTDIR/danio_annotation.gtf
 
 #spike in for CnR
 curl -s https://ftp.ensembl.org/pub/release-109/fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz | gunzip -c $OUTDIR/sacc_refseq.fa
