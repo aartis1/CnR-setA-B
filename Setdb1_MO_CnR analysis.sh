60a35ea6e9df0c0d1b2e017e7fd962f9293b9cee
aartis1 <ara67776@uga.edu>
2023-06-13 13:01:54 -0400

    Update Setdb1_MO_CnR analysis.sh

--- Setdb1_MO_CnR analysis.sh
+++ Setdb1_MO_CnR analysis.sh
@@ -5,13 +5,13 @@ SBATCH --ntasks=1	                                # Single task job
 SBATCH --cpus-per-task=24		                            # Number of cores per task - match this to the num_threads used by BLAST
 SBATCH --mem=60gb			                                # Total memory for job
 SBATCH --time=24:00:00  		                            # Time limit hrs:min:sec
-SBATCH --output=/home/ara67776/work/CnR/Setdb1/error			    # Location of standard output and error log files (replace cbergman with your myid)
+SBATCH --output=/scratch/ara67776/CnR_setAB			    # Location of standard output and error log files (replace cbergman with your myid)
 SBATCH --mail-user=ara67776@uga.edu                    # Where to send mail (replace cbergman with your myid)
 SBATCH --mail-type=ALL                            # Mail events (BEGIN, END, FAIL, ALL)
 
 
 #set output directory
-OUTDIR="/home/ara67776/work/CnR/Setdb1"
+OUTDIR="/scratch/ara67776/CnR_setAB"
 
 #if output directory doesn't exist, create it
 if [ ! -d $OUTDIR ]
