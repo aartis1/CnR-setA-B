024211434420ac2b2ca1bc38ef9cc9b222d42d60
aartis1 <ara67776@uga.edu>
2023-07-11 15:11:05 -0400

    Update Setdb1_MO_CnR analysis.sh

--- Setdb1_MO_CnR analysis.sh
+++ Setdb1_MO_CnR analysis.sh
@@ -27,22 +27,29 @@ OUTDIR="/scratch/ara67776/CnR_setAB"
 # curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/005/845/GCA_000005845.2_ASM584v2/GCA_000005845.2_ASM584v2_genomic.fna.gz | gunzip -c > ecoli_refseq.fa
 # #transfer raw files from local computer to the cluster
 
-if [ ! -d $OUTDIR/trimmed ]
-then
-    echo "Directory $OUTDIR/trimmed exists."
-else
-  mkdir $OUTDIR/trimmed
-fi
+# if [ ! -d $OUTDIR/trimmed ]
+# then
+#     echo "Directory $OUTDIR/trimmed exists."
+# else
+#   mkdir $OUTDIR/trimmed
+# fi
 #trim ends of raw data before adapters are taken off (trimming paired ends)
-module load Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
+# module load Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
 #starting with raw files in $OUTDIR/raw
 # trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/10_gfp_MO_IgG_4_5h_1_S10_L001_R1_001.fastq.gz $OUTDIR/raw/10_gfp_MO_IgG_4_5h_1_S10_L001_R2_001.fastq.gz
 # trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/1_suvAB_MO_K9_4_5h_1_S1_L001_R1_001.fastq.gz $OUTDIR/raw/1_suvAB_MO_K9_4_5h_1_S1_L001_R2_001.fastq.gz
-trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/3_suvAB_MO_K9_4_5h_3_S3_L001_R1_001.fastq.gz $OUTDIR/raw/3_suvAB_MO_K9_4_5h_3_S3_L001_R2_001.fastq.gz
-trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/4_setAB_MO_K9_4_5h_1_S4_L001_R1_001.fastq.gz $OUTDIR/raw/4_setAB_MO_K9_4_5h_1_S4_L001_R2_001.fastq.gz
-trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/5_setAB_MO_K9_4_5h_2_S5_L001_R1_001.fastq.gz $OUTDIR/raw/5_setAB_MO_K9_4_5h_2_S5_L001_R2_001.fastq.gz
-trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/6_setAB_MO_K9_4_5h_3_S6_L001_R1_001.fastq.gz $OUTDIR/raw/6_setAB_MO_K9_4_5h_3_S6_L001_R2_001.fastq.gz
-trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/7_gfp_MO_K9_4_5h_1_S7_L001_R1_001.fastq.gz $OUTDIR/raw/7_gfp_MO_K9_4_5h_1_S7_L001_R2_001.fastq.gz
-trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/8_gfp_MO_K9_4_5h_2_S8_L001_R1_001.fastq.gz $OUTDIR/raw/8_gfp_MO_K9_4_5h_2_S8_L001_R2_001.fastq.gz
-trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/9_gfp_MO_K9_4_5h_3_S9_L001_R1_001.fastq.gz $OUTDIR/raw/9_gfp_MO_K9_4_5h_3_S9_L001_R2_001.fastq.gz
-#aligning trimmed reads to K-Met spike in using bowtie
+# trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/3_suvAB_MO_K9_4_5h_3_S3_L001_R1_001.fastq.gz $OUTDIR/raw/3_suvAB_MO_K9_4_5h_3_S3_L001_R2_001.fastq.gz
+# trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/4_setAB_MO_K9_4_5h_1_S4_L001_R1_001.fastq.gz $OUTDIR/raw/4_setAB_MO_K9_4_5h_1_S4_L001_R2_001.fastq.gz
+# trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/5_setAB_MO_K9_4_5h_2_S5_L001_R1_001.fastq.gz $OUTDIR/raw/5_setAB_MO_K9_4_5h_2_S5_L001_R2_001.fastq.gz
+# trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/6_setAB_MO_K9_4_5h_3_S6_L001_R1_001.fastq.gz $OUTDIR/raw/6_setAB_MO_K9_4_5h_3_S6_L001_R2_001.fastq.gz
+# trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/7_gfp_MO_K9_4_5h_1_S7_L001_R1_001.fastq.gz $OUTDIR/raw/7_gfp_MO_K9_4_5h_1_S7_L001_R2_001.fastq.gz
+# trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/8_gfp_MO_K9_4_5h_2_S8_L001_R1_001.fastq.gz $OUTDIR/raw/8_gfp_MO_K9_4_5h_2_S8_L001_R2_001.fastq.gz
+# trim_galore --fastqc -j 8 --output_dir $OUTDIR/trimmed --paired $OUTDIR/raw/9_gfp_MO_K9_4_5h_3_S9_L001_R1_001.fastq.gz $OUTDIR/raw/9_gfp_MO_K9_4_5h_3_S9_L001_R2_001.fastq.gz
+#checking to see if trimmed reads are real and not empty
+if [ -s $10_gfp_MO_IgG_4_5h_1_S10_L001_R1_001_val_1.fq.gz ] && [ -s $10_gfp_MO_IgG_4_5h_1_S10_L001_R2_001_val_2.fq.gz ]; then
+    echo "  $10_gfp_MO_IgG_4_5h_1_S10_L001_R1_001_val_1.fq.gz and $10_gfp_MO_IgG_4_5h_1_S10_L001_R2_001_val_2.fq.gz found and not empty"
+else
+    echo "  $10_gfp_MO_IgG_4_5h_1_S10_L001_R1_001_val_1.fq.gz and $10_gfp_MO_IgG_4_5h_1_S10_L001_R2_001_val_2.fq.gz was found to either not exist or be empty"
+    echo "exiting"
+    exit 0
+fi
